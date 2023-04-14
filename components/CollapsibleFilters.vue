1ada5f78f0217a914401fc4bc44920281c801e73
Luis Martinez <luismartinezwebdev@gmail.com>
2023-04-14 16:48:27 +0100

    feat: works even mocking promise

--- components/CollapsibleFilters.vue
+++ components/CollapsibleFilters.vue
@@ -2,64 +2,76 @@
 .collapsible-wrapper
   button.toggle-button(@click="toggleAll") {{ allExpanded ? '▼' : '►' }}
 
-  ul.country-list
-    li.country(v-for="(country, cIndex) in countries", :key="country.id")
-      .country-header
-        button.toggle-country(@click="toggleCountry(country)") {{ country.expanded ? '▼' : '►' }}
-        span {{ country.name }}
+  .collapsible-content(v-bind:style="{ maxHeight: allExpanded ? `${allContentHeight}px` : '0' }", ref="allContent")
+    ul.country-list
+      li.country(v-for="country in countries", :key="country.id")
+        .country-header
+          button.toggle-country(@click="toggleCountry(country)") {{ country.expanded ? '▼' : '►' }}
+          span {{ country.name }}
 
-      .collapsible-content(v-bind:style="{ maxHeight: country.expanded ? `${country.contentHeight}px` : '0' }", ref="countryContent")
-        ul.destination-list
-          li.destination(v-for="(destination, dIndex) in country.destinations", :key="destination.id")
-            .destination-header
-              button.toggle-destination(@click="toggleDestination(destination)") {{ destination.expanded ? '▼' : '►' }}
-              span {{ destination.name }}
+        .collapsible-content(v-bind:style="{ maxHeight: country.expanded ? `${country.contentHeight}px` : '0' }", ref="countryContent")
+          ul.destination-list
+            li.destination(v-for="destination in country.destinations", :key="destination.id")
+              .destination-header
+                button.toggle-destination(@click="toggleDestination(destination)") {{ destination.expanded ? '▼' : '►' }}
+                span {{ destination.name }}
 
-            .collapsible-content(v-bind:style="{ maxHeight: destination.expanded ? `${destination.contentHeight}px` : '0' }", ref="destinationContent")
-              ul.zone-list
-                li.zone(v-for="zone in destination.zones", :key="zone.id") {{ zone.name }}
+              .collapsible-content(v-bind:style="{ maxHeight: destination.expanded ? `${destination.contentHeight}px` : '0' }", ref="destinationContent")
+                ul.zone-list
+                  li.zone(v-for="zone in destination.zones", :key="zone.id") {{ zone.name }}
 </template>
 
 <script>
-import countries from "@/data/countries";
-
 export default {
+  name: 'CollapsibleFilters',
+  props: {
+    countries: {
+      type: Array,
+      required: true,
+    },
+  },
+
   data() {
     return {
-    allExpanded: true,
-    countries
+      allExpanded: true,
+      allContentHeight: 0,
     };
   },
   mounted() {
-    this.countries.forEach((country, cIndex) => {
-      this.$set(country, "contentHeight", 0);
-      this.$nextTick(() => {
-        country.contentHeight = this.$refs.countryContent[cIndex].scrollHeight;
-      });
+    this.allContentHeight = this.$refs.allContent.scrollHeight;
 
+    this.countries.forEach((country) => {
+      country.expanded = true;
+      country.contentHeight = this.calculateContentHeight(this.$refs.countryContent, this.countries.indexOf(country));
       country.destinations.forEach((destination, dIndex) => {
-        this.$set(destination, "contentHeight", 0);
-        this.$nextTick(() => {
-          destination.contentHeight = this.$refs.destinationContent[dIndex].scrollHeight;
-        });
+        destination.expanded = true;
+        destination.contentHeight = this.calculateContentHeight(this.$refs.destinationContent, country.destinations.indexOf(destination));
+        destination.parentDestinations = country.destinations;
       });
     });
   },
   methods: {
+    calculateContentHeight(ref, index) {
+      return ref[index] ? ref[index].scrollHeight : 0;
+    },
     toggleAll() {
       this.allExpanded = !this.allExpanded;
-      this.countries.forEach((country) => {
-        country.expanded = this.allExpanded;
-        country.destinations.forEach((destination) => {
-          destination.expanded = this.allExpanded;
-        });
-      });
+      // this.countries.forEach((country) => {
+      //   country.expanded = this.allExpanded;
+      //   country.contentHeight = this.calculateContentHeight(this.$refs.countryContent, this.countries.indexOf(country));
+      //   country.destinations.forEach((destination) => {
+      //     destination.expanded = this.allExpanded;
+      //     destination.contentHeight = this.calculateContentHeight(this.$refs.destinationContent, country.destinations.indexOf(destination));
+      //   });
+      // });
     },
     toggleCountry(country) {
       country.expanded = !country.expanded;
+      country.contentHeight = this.calculateContentHeight(this.$refs.countryContent, this.countries.indexOf(country));
     },
     toggleDestination(destination) {
       destination.expanded = !destination.expanded;
+      destination.contentHeight = this.calculateContentHeight(this.$refs.destinationContent, destination.parentDestinations.indexOf(destination));
     },
   },
 };
@@ -69,9 +81,11 @@ export default {
 li {
   list-style: none;
 }
+
 .collapsible-wrapper {
   overflow: hidden;
 }
+
 .collapsible-content {
   overflow: hidden;
   transition: max-height 0.5s ease;
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,28 @@
 <template lang="pug">
 //- MinimalToggle
-CollapsibleFilters
+CollapsibleFilters(:countries="countries")
 </template>
 
 <script>
+import countries from "@/data/countries";
+
 export default {
   name: 'IndexPage',
+  data () {
+    return {
+      countries,
+    }
+  },
+  async mounted () {
+    // mock fetch request that returns countries
+    const fetchCountries = () =>
+      new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(countries);
+        }, 500);
+      });
+
+    this.countries = await fetchCountries();
+  }
 }
 </script>
