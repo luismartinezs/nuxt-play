f44f8173706508ce0de818a3debec3715f0019e2
Luis Martinez <luismartinezwebdev@gmail.com>
2023-04-14 16:01:13 +0100

    feat: minimal toggle working as expected

---
+++ components/MinimalToggle.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+.collapsible-wrapper
+  button.toggle-button(@click="toggle") {{ expanded ? '▼' : '►' }}
+
+  .collapsible-content(ref="content" v-bind:style="{ maxHeight: expanded ? `${contentHeight}px` : '0' }")
+    ul
+      li Item 1
+      li Item 2
+      li Item 3
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      expanded: false,
+      contentHeight: 0,
+    };
+  },
+  mounted() {
+    this.contentHeight = this.$refs.content.scrollHeight;
+  },
+  methods: {
+    toggle() {
+      this.expanded = !this.expanded;
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  list-style: none;
+}
+.collapsible-wrapper {
+  overflow: hidden;
+  border: 1px solid #ccc;
+}
+.collapsible-content {
+  transition: max-height .5s ease;
+  overflow: hidden;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
-CollapsibleFilters
+MinimalToggle
+//- CollapsibleFilters
 </template>
 
 <script>
